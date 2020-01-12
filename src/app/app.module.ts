890bcdb5e212e2f683661929e7ef9031097ed61c
Roger Lopesierra <rogerlopesierraolivera@gmail.com>
2020-01-12 03:48:49 -0500

    Primer commit app sin effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 import { AppComponent } from './app.component';
+
+// Rutas
+import { AppRoutingModule } from './app-routing.module';
+
+// Modulos personalizados
+import { SharedModule } from './shared/shared.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    UsuariosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,18 @@
+/* "id": 2,
+"email": "janet.weaver@reqres.in",
+"first_name": "Janet",
+"last_name": "Weaver",
+"avatar": "https:// */
+
+export class Usuario {
+	/* public id: number;
+	public first_name: string;
+	public last_name: string;
+	public avatar: string; */
+	constructor(
+		public id: number,
+		public first_name: string,
+		public last_name: string,
+		public avatar: string
+	) {}
+}
